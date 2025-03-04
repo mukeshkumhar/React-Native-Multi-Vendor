4a936d829406e123e83cddfc35332ddc43d3b2b9
Codewhatyouwant <mukeshkumhar906@gmail.com>
2025-03-04 23:02:52 +0530

    Refactor Shop screen to implement category cards and enhance layout with scrollable view

--- screens/bottom.tabs.screen/Shop.js
+++ screens/bottom.tabs.screen/Shop.js
@@ -1,21 +1,97 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image, TouchableOpacity, SafeAreaView, ScrollView } from 'react-native'
 import React from 'react'
 
+
+const categories = [
+    {
+        id: 1,
+        image: require('../../assets/images/photo1.png'),
+        title: 'Category 1',
+    },
+    {
+        id: 2,
+        image: require('../../assets/images/photo1.png'),
+        title: 'Category 2',
+    },
+    {
+        id: 3,
+        image: require('../../assets/images/photo1.png'),
+        title: 'Category 3',
+    },
+    {
+        id: 4,
+        image: require('../../assets/images/photo1.png'),
+        title: 'Category 4',
+    },
+];
+
+const CategoryCard = ({ image, title }) => (
+    <TouchableOpacity style={styles.categoryCard}>
+        <Image source={image} style={styles.categoryImage} />
+        <Text style={styles.categoryTitle}>{title}</Text>
+    </TouchableOpacity>
+);
 const Shop = () => {
     return (
-        <View style={styles.container}>
-            <Text>Shop</Text>
-        </View>
+        <SafeAreaView>
+            <ScrollView>
+                <View style={styles.mainContainer}>
+                    <View style={styles.header}><Text style={styles.headerText}>Categories</Text></View>
+                    <View style={styles.container}>
+
+                        {categories.map((category) => (
+                            <CategoryCard key={category.id} image={category.image} title={category.title} />
+                        ))}
+                    </View>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 
 export default Shop
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
+    mainContainer: {
+        marginTop: 50,
+    },
+    header: {
+        marginTop: 5,
         justifyContent: 'center',
         alignItems: 'center',
+
+    },
+    headerText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    container: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15,
         backgroundColor: '#F5F5F5',
-    }
+    },
+    categoryCard: {
+        width: 120,
+        height: 160,
+        marginTop: 20,
+        marginBottom: 0,
+        marginRight: 5,
+        alignItems: 'center',
+        padding: 0,
+        backgroundColor: 'white',
+        borderRadius: 10,
+    },
+    categoryImage: {
+        width: 120,
+        height: 120,
+        borderRadius: 10,
+    },
+    categoryTitle: {
+        textAlign: 'center',
+        marginTop: 5,
+        marginBottom: 10,
+    },
+
 })
